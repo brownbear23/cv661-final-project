1bb12e3751042eca08688782c53d3a5451a9e4b4
Bill Han <brownbear23@naver.com>
2025-04-26 02:41:02 +0000

    feat: inference demo update

--- src/yolo/inference.py
+++ src/yolo/inference.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 from src.util.drawer.img_drawer import draw_on_image
 
-def run_inference(model_path, source_path, save_predictions=False, output_path="train_out.png"):
+def run_inference(model_path, source_path, save_predictions, output_path):
     # Load the model
     model = YOLO(model_path)
 
@@ -10,24 +10,25 @@ def run_inference(model_path, source_path, save_predictions=False, output_path="
     results = model.predict(source=source_path, save=save_predictions)
 
     detection = results[0]
-    original_image = detection.orig_img.copy()
-    print(detection.names)
-
-    # Draw predictions
-    for box in detection.boxes:
-        cls = int(box.cls[0])
-        cls_name = detection.names[cls]
-        score = round(float(box.conf[0]), 2)
-        bound = box.xyxy[0].tolist()
-        draw_on_image(original_image, cls_name, score, bound)
+    # print(detection.names)
 
     # Save the result image
-    cv2.imwrite(output_path, original_image)
+    if not save_predictions:
+        original_image = detection.orig_img.copy()
+        # Draw predictions
+        for box in detection.boxes:
+            cls = int(box.cls[0])
+            cls_name = detection.names[cls]
+            score = round(float(box.conf[0]), 2)
+            bound = box.xyxy[0].tolist()
+            draw_on_image(original_image, cls_name, score, bound)
+        cv2.imwrite(output_path, original_image)
 
 # Example usage
 if __name__ == "__main__":
     run_inference(
-        model_path="./best.pt",
-        source_path="./train_in.jpeg",
-        save_predictions=True
+        model_path="/workspace/cv661-final-project/runs/detect/new_train8/weights/best.pt",
+        source_path="/workspace/cv661-final-project/src/yolo/0584.jpg",
+        save_predictions=True,
+        output_path=""
     )
