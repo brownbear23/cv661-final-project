d5185645bf9b2a861692657d5cdd2e255daccea4
Bill Han <brownbear23@naver.com>
2025-04-26 02:40:06 +0000

    feat: training module update

--- src/yolo/training.py
+++ src/yolo/training.py
@@ -1,24 +1,41 @@
 from ultralytics import YOLO
+import time
+from datetime import datetime
+import pytz
+
+# Define EST timezone
+est = pytz.timezone('US/Eastern')
+current_time = datetime.now(est)
+current_time_str = current_time.strftime("%y%m%d_%H%M%S")
 
 if __name__ == "__main__":
-    base_models = ["../library/litter-detection/runs/detect/train/yolov8m_100epochs/weights/best.pt",
-                   "../library/YOLO_weights/yolo11m.pt"]
+    start_time = time.time()
+    print(f"Processing: train_{current_time_str}")
 
+    base_models = ["../../library/litter-detection/runs/detect/train/yolov8m_100epochs/weights/best.pt",
+                   "../../library/YOLO_weights/yolo11m.pt"]
 
     # Load the model
     model = YOLO(base_models[0])
 
     # Train the model
     model.train(
-        data="./TACO.yaml",
-        epochs=100,
+        data="HOTACO.yaml",
+        # epochs=100,
+        epochs=2,
         patience=25,
         imgsz=640,
-        device='cpu',
-        name="new_train",
-        pretrained=False,
+        device='0',
+        name="train_"+current_time_str,
+        pretrained=True,
         optimizer='SGD',
     )
 
-    # Validate the model
-    metrics = model.val()
+    # Validate the model (however, this uses the latest epoch)
+    # model.val(
+    #     split="test",
+    #     name="test_"+current_time_str,
+    # )
+
+    end_time = time.time()
+    print(f"Script finished in {end_time - start_time:.2f} seconds at [train_{current_time_str}]")
