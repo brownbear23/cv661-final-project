701cd705a5f3ca45e6e3fccb236c20b54ceada2a
Bill Han <brownbear23@naver.com>
2025-04-27 06:22:31 +0000

    feat: training and testing module update

--- src/yolo/testing.py
+++ src/yolo/testing.py
@@ -8,11 +8,37 @@ current_time = datetime.now(est)
 current_time_str = current_time.strftime("%y%m%d_%H%M%S")
 print(f"Processing: test_best_{current_time_str}")
 
-# Load the best model
-model = YOLO("/workspace/cv661-final-project/runs/detect/train_250425_222350/weights/best.pt")
+NORMAL_TESTING = False
 
-# Validate the best model
-model.val(
-    split='test',  # or split='val' if you want validation set
-    name="test_best_" + current_time_str,
-)
+if NORMAL_TESTING:
+    print("Normal testing for all three models")
+    best_models = ["/workspace/cv661-final-project/runs/detect/m1_yolo8_train_250427_002807/weights/best.pt",
+                   "/workspace/cv661-final-project/runs/detect/m2_single_train_250427_011321/weights/best.pt",
+                   "/workspace/cv661-final-project/runs/detect/m3_seq_stage2_train_250427_011321/weights/best.pt"]
+
+    test_names = ["m1_yolo8_test_",
+                   "m2_single_test_",
+                   "m3_seq_stage2_test_"]
+
+    for best_model, test_name in zip(best_models, test_names):
+        # Load the best model
+        model = YOLO(best_model)
+
+        # Validate the best model
+        model.val(
+            split='test',  # or split='val' if you want validation set
+            name=test_name + current_time_str,
+        )
+
+else:
+    print("m2_single_HOTACO_only_test")
+    # For HOTACO test data set only validation
+    test_name = "m2_single_HOTACO_only_test_"
+    best_model = "/workspace/cv661-final-project/runs/detect/m2_single_train_250427_011321/weights/best.pt"
+    model = YOLO(best_model)
+    model.val(
+        data="/workspace/cv661-final-project/src/yolo/data_yaml/m2_single_HOTACO_only_test.yaml",
+        split='test',
+        name=test_name + current_time_str,
+    )
+
--- src/yolo/training.py
+++ src/yolo/training.py
@@ -12,26 +12,49 @@ if __name__ == "__main__":
     start_time = time.time()
     print(f"Processing: train_{current_time_str}")
 
+
+
     base_models = ["../../library/litter-detection/runs/detect/train/yolov8m_100epochs/weights/best.pt",
-                   "../../library/YOLO_weights/yolo11m.pt"]
+                   "../../library/YOLO_weights/yolo11m.pt",
+                   "../../library/YOLO_weights/yolo11m.pt"
+                   ]
 
-    # Load the model
-    model = YOLO(base_models[0])
+    data_yamls = ["/workspace/cv661-final-project/src/yolo/data_yaml/m1_yolo8.yaml",
+                  "/workspace/cv661-final-project/src/yolo/data_yaml/m2_single.yaml",
+                 "/workspace/cv661-final-project/src/yolo/data_yaml/m3_seq_stage1.yaml"]
 
-    # Train the model
-    model.train(
-        data="HOTACO.yaml",
-        # epochs=100,
-        epochs=2,
-        patience=25,
-        imgsz=640,
-        device='0',
-        name="train_"+current_time_str,
-        pretrained=True,
-        optimizer='SGD',
-    )
+    train_names = ["m1_yolo8_train_", "m2_single_train_", "m3_seq_stage1_train_"]
 
-    # Validate the model (however, this uses the latest epoch)
+
+
+
+    # The first base model should be changed to the most recent m3_seq_stage1_train_*.pt weight
+    # base_models = ["/workspace/cv661-final-project/runs/detect/m3_seq_stage1_train_250427_002807/weights/best.pt"
+    #               ]
+    #
+    # data_yamls = ["/workspace/cv661-final-project/src/yolo/data_yaml/m3_seq_stage2.yaml",
+    #               ]
+    #
+    # train_names = ["m3_seq_stage2_train_",
+    #                ]
+
+
+    for base_model, data_yaml, train_name in zip(base_models, data_yamls, train_names):
+        # print(base_model, data_yaml, train_name)
+        model = YOLO(base_model)
+        model.train(
+            data=data_yaml,
+            epochs=100,
+            workers=8,
+            patience=25,
+            imgsz=640,
+            device='0',
+            name=train_name+current_time_str,
+            pretrained=True,
+            optimizer='SGD',
+         )
+
+    # Validate the model (however, this uses the latest epoch) - DELETE
     # model.val(
     #     split="test",
     #     name="test_"+current_time_str,
